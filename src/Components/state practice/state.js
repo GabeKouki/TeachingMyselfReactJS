d068644a8dd04de51bbcd5673d0de834aa1df043
Gabriel Kouki <gabekouki02@gmail.com>
2023-01-12 23:01:20 -0500

    Fixed function bind's

--- src/Components/state practice/state.js
+++ src/Components/state practice/state.js
@@ -4,7 +4,11 @@ import './counter.css'
 class Button extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {counter: 0}
+        this.state = {counter: 0};
+        this.increaseValue = this.increaseValue.bind(this);
+        this.decrementValue = this.decrementValue.bind(this)
+        this.setValueToZero = this.setValueToZero.bind(this)
+
     }
 
     increaseValue() {
@@ -27,9 +31,9 @@ class Button extends React.Component {
                 <p>in this example I have a class with a constructor that holds this.state.counter which is being updated by an onClick eventListener that invokes a function on this.state.counter</p>
                 <p className="number">{this.state.counter}</p>
                 <div className="buttoncontainer">
-                <button onClick={() => this.increaseValue()}>increment</button>
-                <button onClick={() => this.decrementValue()}>decrement</button>
-                <button onClick={() => this.setValueToZero()}>set to zero</button>
+                <button onClick={this.increaseValue}>increment</button>
+                <button onClick={this.decrementValue}>decrement</button>
+                <button onClick={this.setValueToZero}>set to zero</button>
                 </div>
             </div>
         );
